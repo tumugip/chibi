79eb768c8fc463ab81c387c30cb08aacf4a0a526
TOMIOKA Mayu <21716065@jwu.ac.jp>
2019-10-07 16:14:37 +0900

    rational added  init

--- rational.py
+++ rational.py
@@ -1,13 +1,15 @@
 class Q(object):
-    def __init__(self,a,b):
+    def __init__(self,a,b=1):
         self.a = a
         self.b = b
-    def __repr__(self):    
+    def __repr__(self): 
+        if self.b == 1:
+            return  str(self.a) 
         return f'{self.a}/{self.b}'
 
-
-    #def added gcd(self):
         
 
 q=Q(1,2)
+print(q)
+q=Q(3,1)
 print(q)
