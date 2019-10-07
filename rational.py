720de8979030c477efa17240349712c2454179e6
TOMIOKA Mayu <21716065@jwu.ac.jp>
2019-10-07 16:31:02 +0900

    rational all operators

--- rational.py
+++ rational.py
@@ -6,17 +6,45 @@ class Q(object):
         if self.b == 1:
             return  str(self.a) 
         return f'{self.a}/{self.b}'
-
-    def add(self,q):
+    
+    
+    def __add__(self,q):
         a = self.a
         b = self.b
         c = q.a
         d = q.b
         return Q(a*d+b*c,b*d)
 
+
+    def __sub__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*d-b*c,b*d)
+
+
+    def __mul__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*c,b*d)
+
+    def __truediv__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*d,b*c)
+
+    
+
         
 
 q1=Q(1,2)
 q2=Q(1,3)
-print(q1.add(q2))
-
+print(q1 + q2)
+print(q1 - q2)
+print(q1 * q2)
+print(q1 / q2)
