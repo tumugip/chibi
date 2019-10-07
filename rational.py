806c6722faf0682f028ca49f5d038b60807749cc
TOMIOKA Mayu <21716065@jwu.ac.jp>
2019-10-07 16:51:12 +0900

    rational int Incomplete

--- rational.py
+++ rational.py
@@ -1,20 +1,26 @@
 import math
 class Q(object):
     def __init__(self,a,b=1):
-        self.a = a
-        self.b = b
+        gcd = math.gcd(a,b)
+        self.a = a//gcd
+        self.b = b//gcd
     def __repr__(self): 
         if self.b == 1:
             return  str(self.a) 
         self.c = math.gcd(self.a,self.b)
-        if self.a % self.c ==0 and self.b % self.c == 0:
-            self.d = self.a // self.c
-            self.e = self.b // self.c
-            return f'{self.d}/{self.e}'
+        #if self.a % self.c ==0 and self.b % self.c == 0:
+        #    self.d = self.a // self.c
+        #    self.e = self.b // self.c
+        #    return f'{self.d}/{self.e}'
         return f'{self.a}/{self.b}'
     
     
     def __add__(self,q):
+        if isinstance(q,Q) != True:
+            a = self.a
+            b = self.b
+            c=q
+            d=1
         a = self.a
         b = self.b
         c = q.a
@@ -48,7 +54,8 @@ class Q(object):
 
         
 
-q=Q(2,4)
-print(q)
+
+q1=Q(1,6)
+print(q1+2)
 
 
