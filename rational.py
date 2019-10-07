6b030186c20fa9c766974e0049adc5b287545bc9
TOMIOKA Mayu <21716065@jwu.ac.jp>
2019-10-07 16:09:31 +0900

    rational repr

--- rational.py
+++ rational.py
@@ -2,8 +2,9 @@ class Q(object):
     def __init__(self,a,b):
         self.a = a
         self.b = b
-    #def __repr__(self):
-    #    return str(self.a)
+    def __repr__(self):    
+        return f'{self.a}/{self.b}'
+
 
     #def added gcd(self):
         
