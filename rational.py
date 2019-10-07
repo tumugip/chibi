653aaed4864ec8632260be52f675a3d3a5094d76
TOMIOKA Mayu <21716065@jwu.ac.jp>
2019-10-07 16:04:02 +0900

    rational ver.1

---
+++ rational.py
@@ -0,0 +1,12 @@
+class Q(object):
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    #def __repr__(self):
+    #    return str(self.a)
+
+    #def added gcd(self):
+        
+
+q=Q(1,2)
+print(q)
