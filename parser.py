2eafc06c30cd1525d9b68ec816baeae448817113
TOMIOKA Mayu <21716065@jwu.ac.jp>
2019-10-28 16:48:36 +0900

    output Expr

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from exp import Val,Add,Mul
+from exp import Val,Add,Mul,Sub,Div
 
 
 '''
@@ -22,19 +22,29 @@ def parse(s: str):
 
 def parse(s:str):
     if s.find('+') >0:
-        pos = s.find('+')
+        pos = s.rfind('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1),parse(s2))
+    if s.find('-') > 0:
+        pos = s.rfind('-')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Sub(parse(s1),parse(s2))
     if s.find('*') > 0:
-        pos = s.find('*')
+        pos = s.rfind('*')
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Mul(parse(s1),parse(s2))
+    if s.find('/') > 0:
+        pos = s.rfind('/')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Div(parse(s1),parse(s2))
     return Val(int(s))
     
 
 
-e = parse("1+2*3")
+e = parse("1+6/3")
 print(e,e.eval())
 
