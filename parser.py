8898752b870be04a310557041f22cf8f341633cf
TOMIOKA Mayu <21716065@jwu.ac.jp>
2019-10-28 15:51:25 +0900

    output Val

---
+++ parser.py
@@ -0,0 +1,21 @@
+from exp import Val,Add
+
+
+def parse(s: str):
+    num = int(s)
+    return Val(num)
+
+
+e = parse("123")
+print(e)
+
+s = "123+234"
+pos = s.find('+')　　#記号を探す
+print('pos',pos)
+
+
+s1=s[0:pos]
+s2=s[pos+1:]
+print(s,s1,s2)  #＋記号で分割
+
+
