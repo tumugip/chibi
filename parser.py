899f45386c233af2a15b7a924144fa80b8a7aaef
TOMIOKA Mayu <21716065@jwu.ac.jp>
2019-10-28 16:17:57 +0900

    output Binary

--- parser.py
+++ parser.py
@@ -1,21 +1,36 @@
 from exp import Val,Add
 
 
+'''
 def parse(s: str):
     num = int(s)
     return Val(num)
 
 
-e = parse("123")
+#e = parse("123")
+#print(e)
+
+#s = "123+234"
+#pos = s.find('+')　　#記号を探す
+#print('pos',pos)
+
+#s1=s[0:pos]
+#s2=s[pos+1:]
+#print(s,s1,s2)  #＋記号で分割
+'''
+
+
+def parse(s:str):
+    pos = s.find('+')
+    if pos == -1:
+        num = int(s)
+        return Val(num)
+    else:
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(Val(int(s1)),Val(int(s2)))
+
+e=parse("123+456")
 print(e)
-
-s = "123+234"
-pos = s.find('+')　　#記号を探す
-print('pos',pos)
-
-
-s1=s[0:pos]
-s2=s[pos+1:]
-print(s,s1,s2)  #＋記号で分割
 
 
